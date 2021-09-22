41f42044dfa7d4b34f64948cd963a0590b2f51a5
Joseph Chica <54424231+josephc2195@users.noreply.github.com>
2021-09-22 13:39:28 -0400

    update

--- main.py
+++ main.py
@@ -1,52 +1,52 @@
 import numpy as np
 
-def calculateH(queens):
-        h=0
-        if len(queens) != len(set(queens)):
-                h += len(queens) - len(set(queens))
-
-        for x in range(8):
-                for y in range(8):
-                        if y < 7: 
-                                if board[x][y+1]:
-                                        h+=1
-                        elif y > 0:
-                                if board[x][y-1]:
-                                        h+=1
-        return h
-
 def makeBoard():
         return [[0 for i in range(8)] for x in range(8)]
 
-h = 0
-board = makeBoard()
-qPlacement = []
-npq = np.array(qPlacement)
+def addRandQueens(board):
+        for i in board:
+                y = np.random.randint(0, 8)
+                i[np.random.randint(0,8)] = 1
+                npq = np.append(npq, y)
 
-for i in board:
-        y = np.random.randint(0, 8)
-        i[y] = 1
-        npq = np.append(npq, y)
-        print(i)
-currentH = calculateH(npq)
-print(currentH)
-lowest = npq
-lowH = h
+def vertCollisions(board, x):
+        collisions = 0
+        for i in range(8):
+                if board[i][x]:
+                        collisions += 1
+        if collisions > 0:
+                return True
+def diagnalCollisions(board, x, n):
+        collisions = 0
+        for i in range(8):
+                if x+i < 7 and n+i < 7:
+                        if board[x+i][n+i]:
+                                collisions += 1
+                if x-1 >= 0 and n-i >=0:
+                        if board[x-i][n-i]:
+                                collisions += 1
+                if x+1 < 7 and n-i >=0:
+                        if board[x+i][n-i]:
+                                collisions += 1
+                if x-1 >= 0 and n+i < 7:
+                        if board[x-i][n+i]:
+                                collisions += 1
+        if collisions > 1:
+                return True
 
-for i in range(8):
-        if(i > 0):
-                npq[i] = npq[i]-1
-                if(currentH > calculateH(npq)):
-                        lowH = calculateH(npq)
-                        lowest = npq
-                        
+def countH(board):
+        collisions = 0
+        verts = False
+        diag = False
 
-        elif(i < 7):
-                npq[i] = npq[i]+1
-                if(currentH > calculateH(npq)):
-                        lowH = calculateH(npq)
-                        lowest = npq
+        for i in range(8):
+                for x in range(8):
+                        if board[i][x]:
+                                verts = vertCollisions(board, x)
+                                diag = diagnalCollisions(board, i, x)
+                                if(vers or diag):
+                                        collisions += 1
+        return collisions
 
 
 
-                

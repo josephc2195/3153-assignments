c62289ff803b3d46f81df6c2eb073bbf40bebef5
josephc2195 <chicaj1221@gmail.com>
2021-09-22 15:53:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,62 @@
 import numpy as np
 
-def makeBoard():
-        return [[0 for i in range(8)] for x in range(8)]
+class Queens:
+        def __init__(self):
+                self.board = self.makeBoard()
+                h = self.countH(self.board)
+                print(f'Current H: {h}')
+                self.showBoard(self.board)
 
-def addRandQueens(board):
-        for i in board:
-                y = np.random.randint(0, 8)
-                i[np.random.randint(0,8)] = 1
-                npq = np.append(npq, y)
+        def makeBoard(self):
+                board = [[0 for i in range(8)] for x in range(8)]
+                for i in board:
+                        i[np.random.randint(0, 8)] = 1                
+                return board
 
-def vertCollisions(board, x):
-        collisions = 0
-        for i in range(8):
-                if board[i][x]:
-                        collisions += 1
-        if collisions > 0:
-                return True
-def diagnalCollisions(board, x, n):
-        collisions = 0
-        for i in range(8):
-                if x+i < 7 and n+i < 7:
-                        if board[x+i][n+i]:
-                                collisions += 1
-                if x-1 >= 0 and n-i >=0:
-                        if board[x-i][n-i]:
-                                collisions += 1
-                if x+1 < 7 and n-i >=0:
-                        if board[x+i][n-i]:
-                                collisions += 1
-                if x-1 >= 0 and n+i < 7:
-                        if board[x-i][n+i]:
-                                collisions += 1
-        if collisions > 1:
-                return True
+        def showBoard(self, board):
+                for i in board:
+                        print(i)
 
-def countH(board):
-        collisions = 0
-        verts = False
-        diag = False
-
-        for i in range(8):
-                for x in range(8):
+        def vertCollisions(self, board, x):
+                c = 0
+                for i in range(8):
                         if board[i][x]:
-                                verts = vertCollisions(board, x)
-                                diag = diagnalCollisions(board, i, x)
-                                if(vers or diag):
-                                        collisions += 1
-        return collisions
+                                c += 1
+                if c > 0:
+                        return True
 
+        def diagnalCollisions(self, board, x, n):
+                c = 0
+                for i in range(8):
+                        if x+i < 7 and n+i < 7:
+                                if board[x+i][n+i]:
+                                        c += 1
+                        if x-i >= 0 and n-i >=0:
+                                if board[x-i][n-i]:
+                                        c += 1
+                        if x+i < 7 and n-i >=0:
+                                if board[x+i][n-i]:
+                                        c += 1
+                        if x-i >= 0 and n+i < 7:
+                                if board[x-i][n+i]:
+                                        c += 1
+                if c > 1:
+                        return True
 
+        def countH(self, board):
+                collisions = 0
+                verts = False
+                diag = False
 
+                for i in range(8):
+                        for x in range(8):
+                                if board[i][x]:
+                                        verts = self.vertCollisions(board, x)
+                                        diag = self.diagnalCollisions(board, i, x)
+                                        if(verts or diag):
+                                                collisions += 1
+                return collisions
+
+        
+
+q = Queens()

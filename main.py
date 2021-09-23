7303d0082531b35119225f1bf02648f8f8d5566b
Joseph Chica <54424231+josephc2195@users.noreply.github.com>
2021-09-23 14:27:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 from numpy import random
 class Queens:
 
+        #initializes the class and variables needed for class:
         def __init__(self):
                 self.restarts = 0
                 self.moves = 0
                 self.board = self.makeBoard()
+
+                #gets the current number of heuristic 
                 self.h = self.countH(self.board)
                 print(f'Current H: {self.h}')
                 print('Current State:')
                 self.showBoard(self.board)
-                print('')
+
                 self.actions(self.board, self.moves, self.restarts)
 
+        #creates empty 2d array 
         def makeEmptyBoard(self):
                 return [[0 for i in range(8)] for x in range(8)]
 
+        #creates empty 2d array
         def makeBoard(self):
                 board = [[0 for i in range(8)] for x in range(8)]
+
+                #adds a random 1(represents a queen) to each column
                 for i in range(8):
                         board[random.randint(0, 8)][i] = 1                
                 return board
@@ -25,20 +32,100 @@ class Queens:
                 for i in board:
                         print(i)
 
-        def rowCheck(self, board, x):
-                c = 0
-                for i in range(8):
-                        if board[i][x] == 1:
-                                c +=1
-                if c > 1:
-                        return True
+        def actions(self, board, moves, r):
+                copy_of_board =[]
 
+                #this is where all heuristic values are stored for every possible move
+                hValues = self.makeEmptyBoard()
+                n = 7
+                h = self.countH(board)
+
+                print(f'Neighbors found with lower H: {n}')
+                print('Setting new current state...')
+                print('')
+
+                while(h != 0):
+                        copy_of_board = [row.copy() for row in board]
+                        
+                        for c in range(8):
+                                for i in range(8):
+                                        #making the column with a queen all 0's
+                                        copy_of_board[i][c] = 0
+
+                                for i in range(8):
+                                        #note the queen position in the original board
+                                        if board[i][c] == 1:
+                                                qRow = i
+                                        
+                                        #adds a 1 to the start of the column and 
+                                        #calculates the H value for if the queen moves there
+                                        copy_of_board[i][c] = 1
+                                        hValues[i][c] = self.countH(copy_of_board)
+                                        copy_of_board[i][c] = 0
+                                copy_of_board[qRow][c] = 1
+                        
+                        #checks if there are no neighbors with lower h value.
+                        if self.startOver(hValues, n):
+                                print("Restarting...")
+                                r += 1
+                                board = self.makeBoard()
+                        
+                        #assigns the smallest h value to move the queen to said value
+                        row, col, n = self.getSmallestH(hValues, n, h)
+                       
+                        #changing all values in queens column to 0
+                        for i in range(8):
+                                board[i][col] = 0
+                        
+                        #moves the queen to the smallest h value.
+                        board[row][col] = 1
+                        moves += 1
+                        h = self.countH(board)
+                        
+                        #
+                        if self.countH(board) == 0:
+                                print(f'Current H: {h}')
+                                print('Current State:')
+                                self.showBoard(board)
+                                print('Solution Found!')
+
+                                print(f'State changes: {moves}')
+                                print(f'Restarts: {r}')
+
+                        else:
+                                print(f'Current H: {h}')
+                                print('Current State:')
+                                self.showBoard(board)
+                                print(f'Neighbors found with lower H: {n}')
+                                print('Setting new current state...')
+                                print('')
+
+        def countH(self, board):
+                collisions = 0
+                #iterates through the board to see how many collisions queens have
+                for i in range(8):
+                        for x in range(8):
+                                if board[i][x]:
+                                        col = self.colCheck(board, i)
+                                        row = self.rowCheck(board, x)
+                                        diag = self.diagnalCheck(board, i, x)
+                                        if(col or diag or row):
+                                                collisions += 1
+                return collisions
 
         def colCheck(self, board, x):
                 c = 0
                 for i in range(8):
                         if board[x][i]:
                                 c += 1
+                if c > 1:
+                        return True
+
+        def rowCheck(self, board, x):
+                c = 0
+                for i in range(8):
+                        if board[i][x] == 1:
+                                c +=1
                 if c > 1:
                         return True
 
@@ -58,101 +145,7 @@ class Queens:
                                         return True
 
 
-        def countH(self, board):
-                collisions = 0
-                for i in range(8):
-                        for x in range(8):
-                                if board[i][x]:
-                                        col = self.colCheck(board, i)
-                                        row = self.rowCheck(board, x)
-                                        diag = self.diagnalCheck(board, i, x)
-                                        if(col or diag or row):
-                                                collisions += 1
-                return collisions
-
-        def actions(self, board, moves, r):
-                copyBoard =[]
-                hArray = self.makeEmptyBoard()
-                prevSpot = 0
-                solved = False
-                n = 8
-                h = 0 
-
-                while(solved == False):
-                        colCount = 0
-
-                        
-                        copyBoard = [row.copy() for row in board]
-                                
-                        while colCount < 8:
-                                for i in range(8):
-                                        copyBoard[i][colCount] = 0
-                                for i in range(8):
-                                        if board[i][colCount] == 1:
-                                                prevSpot = i
-                                        copyBoard[i][colCount] = 1
-                                        hArray[i][colCount] = self.countH(copyBoard)
-                                        copyBoard[i][colCount] = 0
-                                copyBoard[prevSpot][colCount] = 1
-                                colCount += 1
-                        if self.determineRestart(hArray, n):
-                                print("Restarting...")
-                                r += 1
-                                board = self.makeBoard()
-                        minCol, n = self.findMinCol(hArray, n, h)
-                        minRow = self.findMinRow(hArray, n, h)
-                        for i in range(8):
-                                board[i][minCol] = 0
-                        board[minRow][minCol] = 1
-                        moves += 1
-                        h = self.countH(board)
-
-                        if self.countH(board) == 0:
-                                print('Current State:')
-                                self.showBoard(board)
-                                print('Solution Found!')
-
-                                print(f'State changes: {moves}')
-                                print(f'Restarts: {r}')
-                                solved = True
-                        else:
-                                print(f'Current H: {h}')
-                                print('Current State:')
-                                self.showBoard(board)
-                                print(f'Neighbors found with lower h: {n}')
-                                print('Setting new current state')
-                                print('')
-
-        def findMinCol(self, hArr, n, h):
-                minCol = 8
-                minVal = 8
-                count = 0
-
-                for i in range(8):
-                        for x in range(8):
-                                if hArr[i][x] < minVal:
-                                        minVal = hArr[i][x]
-                                        minCol = x
-                                if hArr[i][x] < h:
-                                        count += 1
-                n = count
-                both = (minCol, n)
-                return both
-        
-        def findMinRow(self, hArr, n, h):
-                minRow = 8
-                minVal = 8
-
-                for i in range(8):
-                        for x in range(8):
-                                if hArr[i][x] < minVal:
-                                        minVal = hArr[i][x]
-                                        minRow = i  
-                return minRow
-                
-
-
-        def determineRestart(self, hArr, n):
+        def startOver(self, hArr, n):
                 minH = 8 
                 for i in range(8):
                         for x in range(8):
@@ -161,4 +154,24 @@ class Queens:
                 
                 if n == 0:
                         return True
-q = Queens()
+        
+        def getSmallestH(self, hArr, n, h):
+                minH = 8
+                count = 0
+                #iterates through heuristic values to find lowest. 
+                #returns column and row value of the smallest value. 
+                for i in range(8):
+                        for x in range(8):
+                                if hArr[i][x] < minH:
+                                        minH = hArr[i][x]
+                                        col = x
+                                        row = i
+                                #if there is a lower neighbor it adds to the count
+                                if hArr[i][x] < h:
+                                        count += 1
+                #count gets assigned to neighbor.
+                n = count
+                cord = (row, col, n)
+                return cord
+
+Queens()

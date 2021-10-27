14bd7e5c6db91b2f0f728f8ec0330fa7a333ca20
josephc2195 <chicaj1221@gmail.com>
2021-10-27 00:53:56 -0400

    Update main.py

--- a-star/main.py
+++ a-star/main.py
@@ -4,38 +4,48 @@ from numpy import random as ra
 class Star:
         def __init__(self):
 
-                board = np.empty((15, 15), dtype=object)
+                self.board = np.empty((15, 15), dtype=object)
 
                 for i in range(15):
                         for x in range(15):
                                 ten = ra.randint(1, 10)
                                 if ten != 1:
-                                        board[i][x] = 0
+                                        self.board[i][x] = 0
                                 else:
-                                        board[i][x] = 1
+                                        self.board[i][x] = 1
+                print("Here is your starting board:")
+                self.showBoard(self.board)
                 self.startRow, self.startCol = self.getStart()
                 self.goalRow, self.goalCol = self.getGoal()
                 self.openList = [(self.startRow, self.startCol)]
                 self.board[self.startRow][self.startCol] = 'S'
                 self.board[self.goalRow][self.goalCol] = 'G'
-
-                
-                
-                
+                cont = self.confirmBoard(self.board)
+        
+        def showBoard(self, board):
+                print(board)
 
                 
         def getStart(self):
-                row = int(input('Enter the row you want the starting node to be on: '))
-                col = int(input('Enter the column you want the starting node to be on: '))
+                row = int(input('Now, enter the row you want the starting node to be on (1-15): ')) - 1 
+                col = int(input('Enter the column you want the starting node to be on (1-15): ')) - 1 
                 startCoords = (row, col)
                 return startCoords
 
         def getGoal(self):
-                row = input('Enter the row you want the goal node to be on: ')
-                col = input('Enter the column you want the goal node to be on: ')
+                row = int(input('Enter the row you want the goal node to be on (1-15): ')) - 1
+                col = int(input('Enter the column you want the goal node to be on (1-15): ')) -1
                 goalCoords = (row, col)
                 return goalCoords
 
+        def confirmBoard(self, board):
+                print('This is your current board: ')
+                print(board)
+                cont = input('Would you like to continue? ')
+                if cont.lower() == 'yes' or cont.lower() == 'y':
+                        return True
+                else:
+                        return 0
         def getH(self, goalRow, goalCol, startRow, startCol):
                 currentH = 0
                 if goalCol > startCol:
@@ -48,4 +58,5 @@ class Star:
                         currentH += startRow - goalRow
                 print(f'Current H: {currentH}')
 
+Star()
                 
